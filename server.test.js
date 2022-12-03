ba06bede6520417b0fa04c616fb92a30b4152445
Soegidev <soegidev.id@gmail.com>
2022-12-03 20:04:52 +0700

    init

---
+++ server.test.js
@@ -0,0 +1,38 @@
+const app = require('./server') // Link to your server file
+const supertest = require('supertest')
+const request = supertest(app)
+
+
+it('Call the /youtube endpoint', async () => {
+    try {
+        const res = await request.get('/youtube')
+        expect(res.status).toBe(200)
+        expect(res.text).toBe('Hello, youtube indonesia!')
+    } catch (err) {
+    }
+});
+it('Call the / endpoint', async() => {
+    try {
+        const res = await request.get('/')
+        expect(res.status).toBe(200)
+        expect(res.text).toBe('This App is running properly!')
+    } catch (err) {
+    }
+})
+it('Call the /pong endpoint', async() => {
+    try {
+        const res = await request.get('/ping')
+        expect(res.status).toBe(200)
+        expect(res.text).toBe('Pong!')
+    } catch (err) {
+    }
+})
+it('Call the /hello/:name endpoint', async() => {
+    try {
+        const res = await request.get('/hello/Iqbal')
+        expect(res.status).toBe(200)
+        expect(res.body.message).toBe('Hello Iqbal')
+    } catch (err) {
+    }
+})
+  
